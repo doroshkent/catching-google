bdd2f0bff2fc6b972b0a580870303b2f8a5257bf
doroshkent <108677278+doroshkent@users.noreply.github.com>
2024-04-18 12:37:39 +0200

    Add new game features and tests
    The game module was significantly expanded to include a scorekeeping system, more complex movement mechanics for tracking player and Google positions, and rules for interacting with in-game elements. Accompanying tests were also created to ensure that these new mechanics and rules operate as intended.

--- game.js
+++ game.js
@@ -1,9 +1,142 @@
 class Game {
-  #settings = { gridSize: { rows: 3, columns: 3 } }
+  #settings = { gridSize: { rows: 3, columns: 3 }, googleJumpInterval: 2000 }
   #status = 'pending'
   #player1
   #player2
   #google
+  #score = {
+    1: { points: 0 },
+    2: { points: 0 }
+  }
+
+  async start() {
+    if (this.#status === 'pending') {
+      this.#status = 'in-process'
+    }
+    this.#createUnits()
+
+    setInterval(() => {
+      this.#moveGoogle()
+    }, this.#settings.googleJumpInterval)
+  }
+
+  #createUnits() {
+    const positions = [];
+    const maxGridWidth = this.#settings.gridSize.columns
+    const maxGridHeight = this.#settings.gridSize.rows
+
+    const createPlayer = (playerNumber) => {
+      const position = new Position(Position.getNotCrossedPosition(
+        positions,
+        maxGridWidth,
+        maxGridHeight));
+      positions.push(position);
+      return new Player(playerNumber, position);
+    }
+
+    this.#player1 = createPlayer(1)
+    this.#player2 = createPlayer(2)
+
+    const googlePosition = new Position(Position.getNotCrossedPosition(
+      positions,
+      maxGridWidth,
+      maxGridHeight))
+
+    this.#google = new Google(googlePosition)
+  }
+
+  #checkBorders(player, delta) {
+    const boundaryCheck = (newPosition, limit) => {
+      return newPosition < 1 || newPosition > limit;
+    }
+
+    let newPosition = player.position.clone();
+
+    if (delta.x) {
+      newPosition.x += delta.x;
+      return boundaryCheck(newPosition.x, this.#settings.gridSize.columns);
+    }
+
+    newPosition.y += delta.y;
+    return boundaryCheck(newPosition.y, this.#settings.gridSize.rows);
+  }
+
+  #checkOtherPlayer(movingPlayer, otherPlayer, delta) {
+    let newPosition = movingPlayer.position.clone()
+    if (delta.x) newPosition.x += delta.x
+    if (delta.y) newPosition.y += delta.y
+
+    return newPosition.equal(otherPlayer.position)
+  }
+
+  #checkGoogleCatching(player) {
+    if (player.position.equal(this.#google.position)) {
+      this.#score[player.playerNumber].points += 1
+      this.#moveGoogle()
+    }
+  }
+
+  #moveGoogle() {
+    const googlePosition = new Position(Position.getNotCrossedPosition(
+      [ this.#player1.position, this.#player2.position, this.#google.position ],
+      this.#settings.gridSize.columns,
+      this.#settings.gridSize.rows))
+
+    this.#google = new Google(googlePosition)
+  }
+
+  #movePlayer(player, otherPlayer, delta) {
+    const isBorder = this.#checkBorders(player, delta)
+    if (isBorder) return
+
+    const isOtherPlayer = this.#checkOtherPlayer(player, otherPlayer, delta)
+    if (isOtherPlayer) return;
+
+    if (delta.x) player.position.x += delta.x
+    if (delta.y) player.position.y += delta.y
+
+    this.#checkGoogleCatching(player, delta)
+  }
+
+  movePlayer1Right() {
+    const delta = { x: 1 }
+    this.#movePlayer(this.#player1, this.#player2, delta)
+  }
+
+  movePlayer1Left() {
+    const delta = { x: -1 }
+    this.#movePlayer(this.#player1, this.#player2, delta)
+  }
+
+  movePlayer1Up() {
+    const delta = { y: -1 }
+    this.#movePlayer(this.#player1, this.#player2, delta)
+  }
+
+  movePlayer1Down() {
+    const delta = { y: 1 }
+    this.#movePlayer(this.#player1, this.#player2, delta)
+  }
+
+  movePlayer2Right() {
+    const delta = { x: 1 }
+    this.#movePlayer(this.#player2, this.#player1, delta)
+  }
+
+  movePlayer2Left() {
+    const delta = { x: -1 }
+    this.#movePlayer(this.#player2, this.#player1, delta)
+  }
+
+  movePlayer2Up() {
+    const delta = { y: -1 }
+    this.#movePlayer(this.#player2, this.#player1, delta)
+  }
+
+  movePlayer2Down() {
+    const delta = { y: 1 }
+    this.#movePlayer(this.#player2, this.#player1, delta)
+  }
 
   get settings() {
     return this.#settings
@@ -29,35 +162,8 @@ class Game {
     return this.#google
   }
 
-  #createUnits() {
-    const positions = [];
-    const maxWidth = this.#settings.gridSize.rows
-    const maxHeight = this.#settings.gridSize.columns
-
-    const createPlayer = (playerNumber) => {
-      const position = new Position(Position.getNotCrossedPosition(
-        positions,
-        maxWidth,
-        maxHeight));
-      positions.push(position);
-      return new Player(playerNumber, position);
-    }
-
-    const googlePosition = new Position(new Position(Position.getNotCrossedPosition(
-      positions,
-      maxWidth,
-      maxHeight)))
-
-    this.#player1 = createPlayer(1)
-    this.#player2 = createPlayer(2)
-    this.#google = new Google(googlePosition)
-  }
-
-  start() {
-    if (this.#status === 'pending') {
-      this.#status = 'in-process'
-    }
-    this.#createUnits()
+  get score() {
+    return this.#score
   }
 }
 
@@ -104,6 +210,14 @@ class Position {
       ))
 
     return { x, y }
+  }
+
+  clone() {
+    return new Position({ x: this.x, y: this.y });
+  }
+
+  equal(otherPosition) {
+    return this.x === otherPosition.x && this.y === otherPosition.y;
   }
 }
 
--- game.test.js
+++ game.test.js
@@ -1,5 +1,6 @@
 const { Game } = require('./game')
 
+
 describe('game tests', () => {
   it('should set gridSize', () => {
     const game = new Game()
@@ -28,13 +29,60 @@ describe('game tests', () => {
   it('game units should have unique coordinates', () => {
     const game = new Game()
 
-    game.settings = {gridSize: {rows: 1, columns: 3}}
+    game.settings = { gridSize: { rows: 1, columns: 3 } }
 
     game.start()
 
-    const units = [game.player1, game.player2, game.google]
+    const units = [ game.player1, game.player2, game.google ]
     const coordinates = units.map(unit => unit.position)
-    const uniqueCoordinates = [...new Set(coordinates)]
+    const uniqueCoordinates = [ ...new Set(coordinates) ]
     expect(uniqueCoordinates.length).toBe(units.length)
   })
+
+  it('should check google position after jump', async () => {
+    const game = new Game()
+    game.settings = { gridSize: { rows: 1, columns: 3 }, googleJumpInterval: 1000 }
+    await game.start()
+    const prevPosition = game.google.position.clone()
+    delay(1500).then(() => expect(game.google.position.equal(prevPosition)).toBeFalsy())
+  })
+
+  it('should catch google by player1 or player2 for one row', async () => {
+    for (let i = 0; i < 10; i++) {
+      const game = new Game()
+      game.settings = {
+        gridSize: { rows: 1, columns: 3 }
+      }
+
+      await game.start()
+      // possible start positions
+      // p1 g p2 / p1 p2 g / p2 p1 g / p2 g p1 / g p1 p2 / g p2 p1
+      const deltaForPlayer1 = game.google.position.x - game.player1.position.x
+
+      // const prevGooglePosition = game.google.position.clone()
+
+      if (Math.abs(deltaForPlayer1) === 2) {
+        const deltaForPlayer2 = game.google.position.x - game.player2.position.x
+        if (deltaForPlayer2 > 0) game.movePlayer2Right()
+        else game.movePlayer2Left()
+
+        expect(game.score[1].points).toBe(0)
+        expect(game.score[2].points).toBe(1)
+      } else {
+        if (deltaForPlayer1 > 0) game.movePlayer1Right()
+        else game.movePlayer1Left()
+
+        expect(game.score[1].points).toBe(1)
+        expect(game.score[2].points).toBe(0)
+      }
+
+      // expect(game.google.position.equal(prevGooglePosition)).toBeFalsy()
+    }
+  })
 })
+
+const delay = (ms) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms)
+  })
+}
